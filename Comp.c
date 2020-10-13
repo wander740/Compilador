ceabbf7e88e04612386f9918ee3354ac8e2036fa
wanderson ramos dos santos <wandersonramos740@gmail.com>
2020-10-12 22:58:43 -0300

    analise lexica

--- Comp.c
+++ Comp.c
@@ -4,9 +4,24 @@
 //de função
 #include <stdarg.h>
 #include <ctype.h>
+#include <string.h>
 
 #define MAXNAME 30
 #define MAXNUM 5
+#define MAXTOKEN 30
+#define MAXOP 30 //número de operadores permitidos
+#define SYMTBL_SZ 1000
+
+#define KWLIST_SZ 4
+
+enum { KW_IF, KW_ELSE, KW_ENDIF, KW_END, TK_IDENT,
+        TK_NUMBER, TK_OPERATOR };
+
+const char *kwcode = "ilee";
+char *symtbl[SYMTBL_SZ];
+char *kwlist[KWLIST_SZ] = {"IF", "ELSE", "ENDIF", "END"};
+int token;
+char value[MAXTOKEN+1];
 
 char look; /* O caracter lido "antecipadamente" (lookahead) */
 int labelCount; /* Contador usado pelo gerador de rótulos */
@@ -34,8 +49,8 @@ int isAddOp(char c);
 void ident();
 void assignment();
 void skipWhite();
-void getName(char *name);
-void getNum(char *num);
+void getName();
+void getNum();
 void other();
 void program();
 void block();
@@ -60,6 +75,11 @@ void relation();
 int isRelOp(char c);
 int isMulOp(char c);
 void newLine();
+void scan();
+int isOp(char c);
+void getOp();
+void skipComma();
+int lookup(char *s, char *list[], int size);
 
 /* PROGRAMA PRINCIPAL */
 int main()
@@ -69,10 +89,130 @@ int main()
     //assignment();
     //if (look != '\n')
     //    expected("NewLine");
-    program();
+    //program();
     //boolExpression();
+    
+    do {
+        scan(token);
+
+        switch (token) {
+            case 'x':
+                printf("Ident: ");
+                break;
+
+            case '#':
+                printf("Number: ");
+                break;
+
+            case 'i':
+            case 'l':
+            case 'e':
+                printf("Keyword: ");
+                break;
+            default:
+                printf("Operator: ");
+                break;
+
+        }
+
+        printf("%s\n", value);
+        if (value[0] == '\n')
+            newLine();
+
+    } while (strcmp(value, "END") == 0);
+
+    //getName(token);
+    //printf("%d\n", lookup(token, kwlist, KWLIST_SZ));
 
     return 0;
+}
+
+int lookup(char *s, char *list[], int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++) {
+        //se as duas strigs forem iguais
+        if (strcmp(list[i], s) == 0)
+            return i;
+
+    }
+
+    return -1;
+
+}
+
+//trata spaço e virgula
+void skipComma()
+{
+    skipWhite();
+
+    if (look == ',') {
+        nextChar();
+        skipWhite();
+
+    }
+
+}
+
+int isOp(char c)
+{
+    return (strchr("+-*/<>:=", c) != NULL);
+
+}
+
+void getOp()
+{
+    int i;
+
+    if (!isOp(look))
+        expected("Operator");
+
+    for (i = 0; isOp(look) && i < MAXOP; i++) {
+        value[i] = look;
+        nextChar();
+
+    }
+
+    value[i] = '\0';
+    //token = TK_OPERATOR;
+    if (strlen(value) == 1)
+        token = value[0];
+
+    else
+        token = '?';
+
+}
+
+void scan()
+{
+    int kw;
+    while (look == '\n')
+        newLine();
+
+        if (isalpha(look)){
+            getName();
+
+        } else if (isdigit(look)){
+            getNum();
+            //token = TK_NUMBER;
+
+        } else if (isOp(look)){
+            getOp();
+           //token = TK_OPERATOR;
+
+        } else {
+            value[0] = look;
+            value[1] = '\0';
+            //token = TK_OPERATOR;
+
+            token = '?';
+            nextChar();
+
+        }
+        skipWhite();
+        //skipComma();
+
 }
 
 /* gera um novo rótulo único */
@@ -316,46 +456,46 @@ void block(int exitLabel)
 }
 
 // recebe o nome de um identificador
-void getName(char *name)
+void getName()
 {
+    int i, kw;
 
-    int i;
     if (!isalpha(look))
         expected("Name");
 
-    for (i = 0; isalnum(look); i++) {
-        if (i >= MAXNAME)
-            fatal("Identifier too long!");
-            name[i] = toupper(look);
-            nextChar();
-
-    }
-
-    name[i] = '\0';
-    skipWhite();
-
-}
-
-// recebe um número inteiro
-void getNum(char *num)
-{
-
-    int i;
-
-    if (!isdigit(look))
-        expected("Integer");
-
-    for (i = 0; isdigit(look); i++) {
-        if (i >= MAXNUM)
-            fatal("Integer too long!");
-
-        num[i] = look;
+    for (i = 0; isalnum(look) && i < MAXNAME; i++) {
+        value[i] = toupper(look);
         nextChar();
 
     }
 
-    num[i] = '\0';
-    skipWhite();
+    value[i] = '\0';
+    kw = lookup(value, kwlist, KWLIST_SZ);
+
+    if (kw == -1)
+        //token = TK_IDENT;
+         token = 'x';
+    else
+        token = kwcode[kw];
+
+}
+
+// recebe um número inteiro
+void getNum()
+{
+    int i; 
+
+    if (!isdigit(look))
+        expected("Integer");
+
+    for (i = 0; isdigit(look) && i < MAXNUM; i++) {
+        value[i] = look;
+        nextChar();
+
+    }
+    value[i] = '\0';
+    //token = TK_NUMBER;
+    token = '#';
 
 }
 
@@ -651,9 +791,11 @@ void divide()
 
 void skipWhite()
 {
-    while (look == ' ' || look == '\t')
+    //newLine();
+    while (look == ' ' || look == '\t'){
+    //while (isspace(look)){
         nextChar();
-
+    }
 }
 
 /* reconhece uma literal Booleana */
@@ -868,4 +1010,5 @@ void newLine()
 {
     if (look == '\n')
         nextChar();
+        //skipWhite();
 }
